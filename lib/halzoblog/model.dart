d0461167c236419658a41a275c0ca93b192acf18
KateSawada <nuksawada@gmail.com>
2023-09-07 23:31:28 +0900

    不要ファイル削除

--- lib/halzoblog/model.dart
+++
@@ -1,8 +0,0 @@
-/// Model層のクラス ///////////////////////////////////////////////////
-class SampleModelO {
-  // カウントアップ計算を実行し、結果をViewModel層に返す
-  int countUpO(int countNumberO) {
-    countNumberO = countNumberO + 1;
-    return countNumberO;
-  }
-}
